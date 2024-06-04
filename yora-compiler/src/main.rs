56cc2782c20b2c892a3810984f22d5e358b6cf11
vicente410 <brunottvicente@gmail.com>
2024-06-04 16:59:10 +0100

    default name of compiled files now are the same as source

--- yora-compiler/src/main.rs
+++ yora-compiler/src/main.rs
@@ -10,17 +10,23 @@ mod lexer;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let filename: String;
 
     if args.len() != 2 {
-        panic!("Incorrect usage, gain some skill and come back later.");
+        panic!("Incorrect usage.");
+    }
+
+    match &args[1].strip_suffix(".yo") {
+        Some(res) => filename = res.to_string(),
+        None => panic!("Incorrect extension: please use .yo"),
     }
 
     let source = fs::read_to_string(&args[1]).unwrap();
 
-    output(lex(source));
+    output(lex(source), filename);
 }
 
-fn output(tokens: Vec<Token>) {
+fn output(tokens: Vec<Token>, filename: String) {
     let mut buffer = String::new();
 
     buffer.push_str("global _start\n_start:");
@@ -41,23 +47,23 @@ fn output(tokens: Vec<Token>) {
         panic!("Syntax Error");
     }
 
-    let mut file = File::create("a.asm").unwrap();
+    let mut file = File::create(format!("{}.asm", filename)).unwrap();
     file.write_all(&buffer.into_bytes()).unwrap();
 
     Command::new("nasm")
-        .args(["-f elf64", "a.asm"])
+        .args(["-f elf64", &format!("{}.asm", filename)])
         .output()
-        .expect("Failed to execute Nasm");
+        .expect("Failed to execute \"nasm\"");
 
     Command::new("ld")
-        .args(["a.o"])
+        .args(["-o", &filename, &format!("{}.o", filename)])
         .output()
-        .expect("Failed to execute Ld");
+        .expect("Failed to execute \"ld\"");
 
     Command::new("rm")
-        .args(["a.o"])
+        .args([format!("{}.o", filename)])
         .output()
-        .expect("Failed to remove a.o");
+        .expect("Failed to remove object file");
 
     println!("Successfully compiled");
 }
