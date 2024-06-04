2ceede92aaf376db9f30d5eaa281fad7d483ba45
vicente410 <brunottvicente@gmail.com>
2024-06-04 02:33:38 +0100

    fixed gitignore

--- yora-compiler/target/debug/deps/yora_compiler-0407f611aabd0aa1.d
+++
@@ -1,5 +0,0 @@
-/home/bruno/cod/prj/yora-lang/yora-compiler/target/debug/deps/libyora_compiler-0407f611aabd0aa1.rmeta: src/main.rs
-
-/home/bruno/cod/prj/yora-lang/yora-compiler/target/debug/deps/yora_compiler-0407f611aabd0aa1.d: src/main.rs
-
-src/main.rs:
--- yora-compiler/target/debug/deps/yora_compiler-b128f35223ab436a.d
+++
@@ -1,5 +0,0 @@
-/home/bruno/cod/prj/yora-lang/yora-compiler/target/debug/deps/libyora_compiler-b128f35223ab436a.rmeta: src/main.rs
-
-/home/bruno/cod/prj/yora-lang/yora-compiler/target/debug/deps/yora_compiler-b128f35223ab436a.d: src/main.rs
-
-src/main.rs:
