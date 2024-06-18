f8de4bbee7882392dfa11613ba9bce786df56ea3
brotexe <dred.dogui@gmail.com>
2024-06-18 14:09:40 +0100

    changed the way the lexer works and added a simple way of handling comments

--- yora-compiler/src/lexer.rs
+++ yora-compiler/src/lexer.rs
@@ -18,20 +18,33 @@ pub enum Token {
 pub fn lex(source: String) -> Vec<Token> {
     let mut tokens: Vec<Token> = Vec::new();
     let mut buffer: String = String::new();
-    let mut iter = source.chars().peekable();
+    let chars: Vec<char> = source.chars().collect();
+    let len = source.len();
+    let mut i = 0;
 
-    while let Some(c) = iter.next() {
-        if c.is_alphanumeric() {
-            buffer.push(c);
-            if let Some(c) = iter.peek() {
-                if !c.is_alphanumeric() {
-                    tokens.push(get_token(buffer.clone()));
-                    buffer.clear();
+    while i < len {
+        if chars[i].is_alphanumeric() {
+            buffer.push(chars[i]);
+            if i + 1 < len && !chars[i + 1].is_alphanumeric() {
+                tokens.push(get_token(buffer.clone()));
+                buffer.clear();
+            }
+        // Ignore comments
+        } else if i + 1 < len && chars[i] == '/' {
+            if chars[i + 1] == '/' {
+                while i < len && chars[i] != '\n' {
+                    i += 1;
+                }
+            } else if chars[i + 1] == '*' {
+                while i + 1 < len && !(chars[i] == '*' && chars[i + 1] == '/') {
+                    i += 1;
                 }
             }
-        } else if !c.is_whitespace() {
-            tokens.push(get_token(c.to_string()));
+        } else if !chars[i].is_whitespace() {
+            tokens.push(get_token(chars[i].to_string()));
         }
+
+        i += 1;
     }
 
     tokens
