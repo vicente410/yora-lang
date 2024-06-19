11cb739020b9a93c8824aad4c60a5c4846261e32
vicente410 <brunottvicente@gmail.com>
2024-06-19 17:51:40 +0100

    added lib.rs and improved code quality

---
+++ yora-compiler/src/lib.rs
@@ -0,0 +1,150 @@
+use lexer::*;
+use parser::*;
+use std::collections::HashMap;
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use std::process;
+use std::process::Command;
+
+mod ir_generation;
+mod lexer;
+mod parser;
+
+#[derive(Eq, Debug, Hash, PartialEq)]
+pub enum Flag {
+    Output,
+    Debug(DebugOptions),
+    Assembly,
+}
+
+#[derive(Eq, Debug, Hash, PartialEq)]
+pub enum DebugOptions {
+    Tokens,
+    Ast,
+}
+
+pub struct Compiler {
+    filename: String,
+    asm_name: String,
+    obj_name: String,
+    exec_name: String,
+    flags: HashMap<Flag, Option<String>>,
+}
+
+impl Compiler {
+    pub fn new() -> Compiler {
+        Compiler {
+            filename: String::new(),
+            asm_name: String::new(),
+            obj_name: String::new(),
+            exec_name: String::new(),
+            flags: HashMap::new(),
+        }
+    }
+
+    pub fn set_flags(&mut self, flags: HashMap<Flag, Option<String>>) {
+        self.flags = flags;
+    }
+
+    pub fn set_filenames(&mut self, filename: &String) {
+        self.filename = filename.to_string();
+        self.asm_name = if self.flags.contains_key(&Flag::Assembly) {
+            if self.flags.contains_key(&Flag::Output) {
+                if let Some(string) = &self.flags[&Flag::Output] {
+                    string.clone()
+                } else {
+                    panic!();
+                }
+            } else {
+                format!("{}.asm", filename)
+            }
+        } else {
+            Compiler::get_tmpfile_path()
+        };
+        self.obj_name = Compiler::get_tmpfile_path();
+        self.exec_name = if self.flags.contains_key(&Flag::Output) {
+            if let Some(string) = &self.flags[&Flag::Output] {
+                string.clone()
+            } else {
+                panic!();
+            }
+        } else {
+            filename.clone()
+        };
+    }
+
+    pub fn compile(&self) {
+        let source = fs::read_to_string(format!("{}.yr", self.filename)).unwrap();
+        let assembly = self.get_assembly(source);
+
+        let mut asm_file = File::create(&self.asm_name).unwrap();
+        asm_file.write_all(&assembly.into_bytes()).unwrap();
+
+        if self.flags.contains_key(&Flag::Assembly) {
+            process::exit(0);
+        }
+
+        self.assemble();
+        self.link();
+        self.remove_tmpfiles();
+    }
+
+    fn get_assembly(&self, source: String) -> String {
+        let tokens = lex(source);
+
+        if self.flags.contains_key(&Flag::Debug(DebugOptions::Tokens)) {
+            dbg!(&tokens);
+            process::exit(0);
+        }
+
+        let ast = parse(tokens);
+
+        if self.flags.contains_key(&Flag::Debug(DebugOptions::Ast)) {
+            dbg!(&ast);
+            process::exit(0);
+        }
+
+        "".to_string()
+    }
+
+    fn assemble(&self) {
+        Command::new("nasm")
+            .args(["-felf64", "-o", &self.obj_name, &self.asm_name])
+            .output()
+            .expect("Failed to execute \"nasm\"");
+    }
+
+    fn link(&self) {
+        Command::new("ld")
+            .args(["-o", &self.exec_name, &self.obj_name])
+            .output()
+            .expect("Failed to execute \"ld\"");
+    }
+
+    fn get_tmpfile_path() -> String {
+        String::from_utf8(
+            Command::new("mktemp")
+                .output()
+                .expect("Failed to create temporary file.")
+                .stdout,
+        )
+        .unwrap()
+        .trim()
+        .to_string()
+    }
+
+    fn remove_tmpfiles(&self) {
+        Compiler::remove_file(&self.obj_name);
+        if !self.flags.contains_key(&Flag::Assembly) {
+            Compiler::remove_file(&self.asm_name);
+        }
+    }
+
+    fn remove_file(filename: &String) {
+        Command::new("rm")
+            .args([filename])
+            .output()
+            .expect("Failed to remove file");
+    }
+}
--- yora-compiler/src/main.rs
+++ yora-compiler/src/main.rs
@@ -1,51 +1,16 @@
-//use ir_generation::*;
-use lexer::*;
-use parser::*;
 use std::collections::HashMap;
 use std::env;
-use std::fs;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
-use std::process::Command;
-
-mod ir_generation;
-mod lexer;
-mod parser;
-
-#[derive(Eq, Debug, Hash, PartialEq)]
-enum Flag {
-    Output,
-    Debug(DebugOptions),
-    Assembly,
-}
-
-#[derive(Eq, Debug, Hash, PartialEq)]
-enum DebugOptions {
-    Tokens,
-    Ast,
-}
+use yora_compiler::*;
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
+    let mut compiler = Compiler::new();
     let (filename, flags) = parse_args(&mut args);
 
-    let source = fs::read_to_string(format!("{}.yr", filename)).unwrap();
-    let assembly = compile(source, &flags);
-
-    let (asm_name, obj_name, exec_name) = get_filenames(filename, &flags);
-
-    let mut asm_file = File::create(&asm_name).unwrap();
-    asm_file.write_all(&assembly.into_bytes()).unwrap();
-
-    if flags.contains_key(&Flag::Assembly) {
-        process::exit(0);
-    }
-
-    assemble(&asm_name, &obj_name);
-    link(&obj_name, &exec_name);
-
-    remove_tmpfiles(asm_name, obj_name, flags);
+    compiler.set_filenames(&filename);
+    compiler.set_flags(flags);
+    compiler.compile();
 }
 
 fn parse_args(args: &mut Vec<String>) -> (String, HashMap<Flag, Option<String>>) {
@@ -76,6 +41,17 @@ fn parse_args(args: &mut Vec<String>) -> (String, HashMap<Flag, Option<String>>)
                 )
             }
             "-s" | "--assembly" => (Flag::Assembly, None),
+            "-h" | "--help" => {
+                print!(
+                    "Usage: yora-compiler [options] file\n\
+                        Option:\n\
+                        \t-h, --help\t\tDisplay this message\n\
+                        \t-o, --output <filename>\tWrite output to <filename>\n\
+                        \t-d, --debug [tokens, ast]\tPrints debug information\n\
+                        \t-s, --assembly\t\tCompile only; do not assemble or link\n"
+                );
+                process::exit(0);
+            }
             _ => panic!("Incorrect usage"),
         };
 
@@ -85,96 +61,4 @@ fn parse_args(args: &mut Vec<String>) -> (String, HashMap<Flag, Option<String>>)
     }
 
     (filename, flags)
-}
-
-fn compile(source: String, flags: &HashMap<Flag, Option<String>>) -> String {
-    let tokens = lex(source);
-
-    if flags.contains_key(&Flag::Debug(DebugOptions::Tokens)) {
-        dbg!(&tokens);
-        process::exit(0);
-    }
-
-    let ast = parse(tokens);
-
-    if flags.contains_key(&Flag::Debug(DebugOptions::Ast)) {
-        dbg!(&ast);
-        process::exit(0);
-    }
-
-    "".to_string()
-    //codegen(ast)
-}
-
-fn get_filenames(
-    filename: String,
-    flags: &HashMap<Flag, Option<String>>,
-) -> (String, String, String) {
-    let asm_name = if flags.contains_key(&Flag::Assembly) {
-        if flags.contains_key(&Flag::Output) {
-            if let Some(string) = &flags[&Flag::Output] {
-                string.clone()
-            } else {
-                panic!();
-            }
-        } else {
-            format!("{}.asm", filename)
-        }
-    } else {
-        get_tmpfile_path()
-    };
-
-    let obj_name = get_tmpfile_path();
-
-    let exec_name = if flags.contains_key(&Flag::Output) {
-        if let Some(string) = &flags[&Flag::Output] {
-            string.clone()
-        } else {
-            panic!();
-        }
-    } else {
-        filename
-    };
-
-    (asm_name, obj_name, exec_name)
-}
-
-fn assemble(asm_name: &String, obj_name: &String) {
-    Command::new("nasm")
-        .args(["-felf64", "-o", obj_name, asm_name])
-        .output()
-        .expect("Failed to execute \"nasm\"");
-}
-
-fn link(obj_name: &String, exec_name: &String) {
-    Command::new("ld")
-        .args(["-o", exec_name, obj_name])
-        .output()
-        .expect("Failed to execute \"ld\"");
-}
-
-fn get_tmpfile_path() -> String {
-    String::from_utf8(
-        Command::new("mktemp")
-            .output()
-            .expect("Failed to create temporary file.")
-            .stdout,
-    )
-    .unwrap()
-    .trim()
-    .to_string()
-}
-
-fn remove_tmpfiles(asm_name: String, obj_name: String, flags: HashMap<Flag, Option<String>>) {
-    remove_file(obj_name);
-    if !flags.contains_key(&Flag::Assembly) {
-        remove_file(asm_name);
-    }
-}
-
-fn remove_file(filename: String) {
-    Command::new("rm")
-        .args([filename])
-        .output()
-        .expect("Failed to remove file");
 }
