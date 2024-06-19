3f9035571cf9118ace61976a80ae3ae2cc803e5b
brotexe <dred.dogui@gmail.com>
2024-06-19 17:53:02 +0100

    started intermediate representation generation implementation

--- yora-compiler/src/ir_generation.rs
+++ yora-compiler/src/ir_generation.rs
@@ -1,23 +1,94 @@
-/*use crate::parser::*;
+use core::panic;
 
-enum InterRepr {
-    Exit(Value, Value),
-    Add(Value, Value, Value),
-    Sub(Value, Value, Value),
-    Mul(Value, Value, Value),
-    Div(Value, Value, Value),
-    Rem(Value, Value, Value),
+use crate::parser::Expression;
+
+#[derive(Debug, PartialEq)]
+enum Ir {
+    Exit(String),
+    Assign(String, String),
+    Add(String, String),
+    Sub(String, String),
+    Mul(String, String),
+    Div(String, String),
+    Mod(String, String),
 }
 
-pub fn generate_ir(ast: Vec<Expression>) -> Vec<InterRepr> {
-    let inter_repr = Vec::new();
-    let num_tmp = 0;
+pub fn generate_ir(ast: Vec<Expression>) -> Vec<Ir> {
+    let mut inter_repr = Vec::new();
+    let mut tmp_vec = Vec::new();
+    let mut num_tmp = 0;
 
     for expr in ast {
-        inter_repr.push(match expr {
-            Expression::Exit() => InterRepr::Exit((), ()),
-        })
+        get_value(expr, &mut tmp_vec, &mut num_tmp);
+        inter_repr.append(&mut tmp_vec);
+        tmp_vec.clear();
     }
 
     inter_repr
-}*/
+}
+
+fn get_value(expr: Expression, tmp_vec: &mut Vec<Ir>, num_tmp: &mut u32) -> String {
+    match expr {
+        Expression::Exit(val) => {
+            *num_tmp += 1;
+            let arg = get_value(*val, tmp_vec, num_tmp);
+            tmp_vec.push(Ir::Exit(arg.clone()));
+            arg
+        }
+
+        Expression::Add(dest, ref src)
+        | Expression::Sub(dest, ref src)
+        | Expression::Mul(dest, ref src)
+        | Expression::Div(dest, ref src)
+        | Expression::Mod(dest, ref src) => {
+            let arg1 = get_value(*dest, tmp_vec, num_tmp);
+            let arg2 = get_value(*src, tmp_vec, num_tmp);
+            get_operation(&expr, arg1.clone(), arg2);
+            arg1
+        }
+        Expression::Integer(val) => {
+            *num_tmp += 1;
+            tmp_vec.push(Ir::Assign(format!("t{num_tmp}"), val.to_string()));
+            val.to_string()
+        }
+    }
+}
+
+fn get_operation(operation: &Expression, arg1: String, arg2: String) -> Ir {
+    match operation {
+        Expression::Add(_, _) => Ir::Add(arg1, arg2),
+        Expression::Sub(_, _) => Ir::Sub(arg1, arg2),
+        Expression::Mul(_, _) => Ir::Mul(arg1, arg2),
+        Expression::Div(_, _) => Ir::Div(arg1, arg2),
+        Expression::Mod(_, _) => Ir::Mod(arg1, arg2),
+        _ => panic!("Unexpected operation."),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ir_generation() {
+        let input = vec![Expression::Exit(Box::new(Expression::Add(
+            Box::new(Expression::Integer("2".to_string())),
+            Box::new(Expression::Integer("3".to_string())),
+        )))];
+
+        let output = vec![
+            Ir::Assign("t1".to_string(), "2".to_string()),
+            Ir::Add("t1".to_string(), "3".to_string()),
+            Ir::Exit("t1".to_string()),
+        ];
+        assert_eq!(generate_ir(input), output);
+    }
+
+    #[test]
+    fn test_integer() {
+        let input = vec![Expression::Integer("2".to_string())];
+        let output = vec![Ir::Assign("t1".to_string(), "2".to_string())];
+
+        assert_eq!(generate_ir(input), output);
+    }
+}
