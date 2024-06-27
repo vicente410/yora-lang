f5487c27788136cda572c7ca8bdae14904ce747e
brotexe <dred.dogui@gmail.com>
2024-06-27 22:36:17 +0100

    improved code readabilty in semantic analyzer

--- yora-compiler/src/semantic_analyzer.rs
+++ yora-compiler/src/semantic_analyzer.rs
@@ -1,18 +1,49 @@
 use crate::parser::*;
-use std::{collections::HashMap, ops::Deref, process};
+use std::{
+    cmp::Ordering,
+    collections::{BTreeSet, HashMap},
+    ops::Deref,
+    process,
+};
 
-enum ErrorTypes {
-    UndeclaredVariable(String, usize, usize),
-    NotAnIdentifier(String, usize, usize),
-    CannotMakeOperation(String, String, usize, usize),
-    NotACondition(usize, usize),
-    InvalidComparison(String, String, usize, usize),
-    InvalidExitCode(usize, usize),
+#[derive(PartialEq, Eq)]
+enum ErrorKind {
+    UndeclaredVariable { var: String },
+    CannotMakeOperation { type1: String, type2: String },
+    NotACondition,
+    InvalidAssignment { type1: String, type2: String },
+    InvalidComparison { type1: String, type2: String },
+    InvalidExitCode,
+}
+
+#[derive(PartialEq, Eq)]
+struct Error {
+    kind: ErrorKind,
+    line: usize,
+    column: usize,
+}
+
+impl Ord for Error {
+    fn cmp(&self, other: &Self) -> Ordering {
+        (self.line, &self.column).cmp(&(other.line, &other.column))
+    }
+}
+
+impl PartialOrd for Error {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Error {
+    fn new(kind: ErrorKind, line: usize, column: usize) -> Error {
+        Error { kind, line, column }
+    }
 }
 
 pub fn analyze(ast: &Vec<Expression>) {
     let mut variables: HashMap<String, String> = HashMap::new();
-    let mut errors: Vec<ErrorTypes> = Vec::new();
+    let mut errors: BTreeSet<Error> = BTreeSet::new();
 
     for expr in ast {
         analyze_expression(expr, &mut variables, &mut errors);
@@ -30,63 +61,27 @@ pub fn analyze(ast: &Vec<Expression>) {
 fn analyze_expression(
     expr: &Expression,
     vars: &mut HashMap<String, String>,
-    errors: &mut Vec<ErrorTypes>,
+    errors: &mut BTreeSet<Error>,
 ) {
     match &expr.kind {
         ExpressionKind::Declare(ref dest, ref src) => {
             analyze_expression(src, vars, errors);
-
-            let dest_val = get_value(dest);
-
-            if get_type(src) != "bool" {
-                let src_val = get_value(src);
-
-                if !vars.contains_key(&src_val) && get_type(src) == "id" {
-                    errors.push(ErrorTypes::UndeclaredVariable(
-                        src_val, src.line, src.column,
-                    ));
-                }
-            }
-
-            if !vars.contains_key(&dest_val) && get_type(dest) == "id" {
-                vars.insert(dest_val, get_type(src));
-            } else {
-                errors.push(ErrorTypes::NotAnIdentifier(
-                    dest_val,
-                    dest.line,
-                    dest.column,
-                ));
-            }
+            vars.insert(get_value(dest, vars, errors), get_type(src, vars, errors));
         }
         ExpressionKind::Assign(ref dest, ref src) => {
             analyze_expression(src, vars, errors);
 
-            let dest_val = get_value(dest);
+            let type1 = get_type(src, vars, errors);
+            let type2 = get_type(dest, vars, errors);
 
-            if get_type(src) != "bool" {
-                let src_val = get_value(src);
-
-                if !vars.contains_key(&src_val) && get_type(src) == "id" {
-                    errors.push(ErrorTypes::UndeclaredVariable(
-                        src_val, src.line, src.column,
-                    ));
-                }
-            }
-            if !vars.contains_key(&dest_val) && get_type(dest) == "id" {
-                errors.push(ErrorTypes::UndeclaredVariable(
-                    dest_val,
-                    dest.line,
-                    dest.column,
-                ));
-            } else if !vars.contains_key(&dest_val) {
-                errors.push(ErrorTypes::NotAnIdentifier(
-                    dest_val,
-                    dest.line,
+            if type1 != type2 {
+                errors.insert(Error::new(
+                    ErrorKind::InvalidAssignment { type1, type2 },
+                    src.line,
                     dest.column,
                 ));
             }
         }
-
         ExpressionKind::Add(ref dest, ref src)
         | ExpressionKind::Sub(ref dest, ref src)
         | ExpressionKind::Mul(ref dest, ref src)
@@ -95,54 +90,14 @@ fn analyze_expression(
             analyze_expression(src, vars, errors);
             analyze_expression(dest, vars, errors);
 
-            let src_val = get_value(src);
-            let dest_val = get_value(dest);
+            let type1 = get_type(src, vars, errors);
+            let type2 = get_type(dest, vars, errors);
 
-            if !vars.contains_key(&src_val) && get_type(src) == "id" {
-                errors.push(ErrorTypes::UndeclaredVariable(
-                    src_val.clone(),
-                    src.line,
-                    src.column,
-                ));
-            }
-
-            if !vars.contains_key(&dest_val) && get_type(dest) == "id" {
-                errors.push(ErrorTypes::UndeclaredVariable(
-                    dest_val.clone(),
-                    dest.line,
-                    dest.column,
-                ));
-            }
-
-            if vars.contains_key(&src_val) && vars[&src_val] != "int" {
-                errors.push(ErrorTypes::CannotMakeOperation(
-                    vars[&src_val].clone(),
-                    "int".to_string(),
-                    src.line,
-                    src.column,
-                ));
-            } else if get_type(src) != "id" && get_type(src) != "int" {
-                errors.push(ErrorTypes::CannotMakeOperation(
-                    get_type(src),
-                    "int".to_string(),
-                    src.line,
-                    src.column,
-                ));
-            }
-
-            if vars.contains_key(&dest_val) && vars[&dest_val] != "int" {
-                errors.push(ErrorTypes::CannotMakeOperation(
-                    vars[&dest_val].clone(),
-                    "int".to_string(),
-                    dest.line,
-                    dest.column,
-                ));
-            } else if get_type(dest) != "id" && get_type(dest) != "int" {
-                errors.push(ErrorTypes::CannotMakeOperation(
-                    get_type(dest),
-                    "int".to_string(),
-                    dest.line,
-                    dest.column,
+            if type1 != "int" || type2 != "int" {
+                errors.insert(Error::new(
+                    ErrorKind::CannotMakeOperation { type1, type2 },
+                    expr.line,
+                    expr.column,
                 ));
             }
         }
@@ -152,15 +107,12 @@ fn analyze_expression(
             }
         }
         ExpressionKind::If(ref cond, ref seq) => {
-            if get_type(cond) != "bool" && get_type(cond) != "id"
-                || (get_type(cond) == "id" && vars[&get_value(cond)] != "bool")
-            {
-                errors.push(ErrorTypes::NotACondition(cond.line, cond.column));
+            if get_type(cond, vars, errors) != "bool" {
+                errors.insert(Error::new(ErrorKind::NotACondition, cond.line, cond.column));
             }
             analyze_expression(cond, vars, errors);
             analyze_expression(seq, vars, errors);
         }
-
         ExpressionKind::Eq(ref cmp1, ref cmp2)
         | ExpressionKind::Neq(ref cmp1, ref cmp2)
         | ExpressionKind::Lt(ref cmp1, ref cmp2)
@@ -170,43 +122,12 @@ fn analyze_expression(
             analyze_expression(cmp1, vars, errors);
             analyze_expression(cmp2, vars, errors);
 
-            let val1 = get_value(cmp1);
-            let val2 = get_value(cmp2);
+            let type1 = get_type(cmp1, vars, errors);
+            let type2 = get_type(cmp2, vars, errors);
 
-            if vars.contains_key(&val1) && vars.contains_key(&val2) && vars[&val1] != vars[&val2] {
-                errors.push(ErrorTypes::InvalidComparison(
-                    vars[&val1].clone(),
-                    vars[&val2].clone(),
-                    cmp1.line,
-                    cmp1.column,
-                ));
-            } else if vars.contains_key(&val1)
-                && !vars.contains_key(&val2)
-                && vars[&val1] != get_type(cmp2)
-            {
-                errors.push(ErrorTypes::InvalidComparison(
-                    vars[&val1].clone(),
-                    get_type(cmp2),
-                    cmp1.line,
-                    cmp1.column,
-                ));
-            } else if !vars.contains_key(&val1)
-                && vars.contains_key(&val2)
-                && get_type(cmp1) != vars[&val2]
-            {
-                errors.push(ErrorTypes::InvalidComparison(
-                    get_type(cmp1),
-                    vars[&val2].clone(),
-                    cmp1.line,
-                    cmp1.column,
-                ));
-            } else if !vars.contains_key(&val1)
-                && !vars.contains_key(&val2)
-                && get_type(cmp1) != get_type(cmp2)
-            {
-                errors.push(ErrorTypes::InvalidComparison(
-                    get_type(cmp1),
-                    get_type(cmp2),
+            if type1 != type2 {
+                errors.insert(Error::new(
+                    ErrorKind::InvalidComparison { type1, type2 },
                     cmp1.line,
                     cmp1.column,
                 ));
@@ -215,19 +136,9 @@ fn analyze_expression(
         ExpressionKind::Loop(ref seq) => analyze_expression(seq, vars, errors),
         ExpressionKind::Exit(ref val) => {
             analyze_expression(val, vars, errors);
-            if (get_type(val) == "id"
-                && vars.contains_key(&get_value(val))
-                && vars[&get_value(val)] != "int")
-                || (get_type(val) != "id" && get_type(val) != "int")
-            {
-                errors.push(ErrorTypes::InvalidExitCode(val.line, val.column));
-            }
-            if get_type(val) == "id" && !vars.contains_key(&get_value(val)) {
-                errors.push(ErrorTypes::NotAnIdentifier(
-                    get_value(val),
-                    val.line,
-                    val.column,
-                ));
+
+            if get_type(val, vars, errors) != "int" {
+                errors.insert(Error::new(ErrorKind::InvalidExitCode, val.line, val.column));
             }
         }
         ExpressionKind::Break
@@ -239,7 +150,11 @@ fn analyze_expression(
         | ExpressionKind::Or(..) => {}
     }
 }
-fn get_value(expr: &Expression) -> String {
+fn get_value(
+    expr: &Expression,
+    vars: &HashMap<String, String>,
+    errors: &mut BTreeSet<Error>,
+) -> String {
     match expr.kind {
         ExpressionKind::Exit(..) => {
             println!("Can't evaluate exit");
@@ -254,18 +169,20 @@ fn get_value(expr: &Expression) -> String {
         | ExpressionKind::Sub(ref dest, _)
         | ExpressionKind::Mul(ref dest, _)
         | ExpressionKind::Div(ref dest, _)
-        | ExpressionKind::Mod(ref dest, _) => get_value(dest),
+        | ExpressionKind::Mod(ref dest, _) => get_value(dest, vars, errors),
         ExpressionKind::IntLit(ref int) => int.to_string(),
         ExpressionKind::BoolLit(ref bool) => bool.to_string(),
         ExpressionKind::Identifier(ref id) => id.to_string(),
         ExpressionKind::If(ref seq, ..) | ExpressionKind::Loop(ref seq) => {
             match &seq.deref().kind {
-                ExpressionKind::Sequence(sequence) => get_type(&sequence[sequence.len() - 1]),
+                ExpressionKind::Sequence(sequence) => {
+                    get_type(&sequence[sequence.len() - 1], vars, errors)
+                }
                 _ => panic!("Sequence in the if is not a sequence."),
             }
         }
         ExpressionKind::Break => "".to_string(),
-        ExpressionKind::Sequence(ref seq) => get_type(&seq[seq.len() - 1]),
+        ExpressionKind::Sequence(ref seq) => get_type(&seq[seq.len() - 1], vars, errors),
         _ => {
             dbg!(&expr.kind);
             panic!("Invalid expression")
@@ -273,9 +190,26 @@ fn get_value(expr: &Expression) -> String {
     }
 }
 
-fn get_type(expr: &Expression) -> String {
-    match expr.kind {
-        ExpressionKind::Identifier(..) => "id".to_string(),
+fn get_type(
+    expr: &Expression,
+    vars: &HashMap<String, String>,
+    errors: &mut BTreeSet<Error>,
+) -> String {
+    match &expr.kind {
+        ExpressionKind::Identifier(id) => {
+            if vars.contains_key(id) {
+                (*vars[id]).to_string()
+            } else {
+                errors.insert(Error::new(
+                    ErrorKind::UndeclaredVariable {
+                        var: id.to_string(),
+                    },
+                    expr.line,
+                    expr.column,
+                ));
+                "null".to_string()
+            }
+        }
         ExpressionKind::IntLit(..) => "int".to_string(),
         ExpressionKind::BoolLit(..)
         | ExpressionKind::Eq(..)
@@ -288,7 +222,7 @@ fn get_type(expr: &Expression) -> String {
         | ExpressionKind::Sub(ref dest, ..)
         | ExpressionKind::Mul(ref dest, ..)
         | ExpressionKind::Div(ref dest, ..)
-        | ExpressionKind::Mod(ref dest, ..) => get_type(dest),
+        | ExpressionKind::Mod(ref dest, ..) => get_type(dest, vars, errors),
         _ => {
             dbg!(&expr);
             panic!("Not a valid type")
@@ -296,25 +230,43 @@ fn get_type(expr: &Expression) -> String {
     }
 }
 
-fn print_error(error: &ErrorTypes) {
-    match error {
-        ErrorTypes::UndeclaredVariable(var, line, column) => {
-            println!("Variable with name {var} undeclared [line {line}: column {column}]")
+fn print_error(error: &Error) {
+    match &error.kind {
+        ErrorKind::UndeclaredVariable { var } => {
+            println!(
+                "Variable with name {var} undeclared [line {}: column {}]",
+                error.line, error.column
+            )
         }
-        ErrorTypes::NotAnIdentifier(id, line, column) => {
-            println!("{id} is not an identifier [line {line}: column {column}]")
+        ErrorKind::CannotMakeOperation { type1, type2 } => {
+            println!(
+                "Can't make an operation between ints, types {type1} and {type2} were given[line {}: column {}]",
+                error.line, error.column
+            )
         }
-        ErrorTypes::CannotMakeOperation(type1, type2, line, column) => {
-            println!("Can't make an operation between type {type1} and {type2} [line {line}: column {column}]")
+        ErrorKind::NotACondition => {
+            println!(
+                "Invalid condition for if statement [line {}: column {}]",
+                error.line, error.column
+            )
         }
-        ErrorTypes::NotACondition(line, column) => {
-            println!("Invalid condition for if statement [line {line}: column {column}]")
+        ErrorKind::InvalidComparison { type1, type2 } => {
+            println!(
+                "Can't compare type {type1} with type {type2} [line {}: column {}]",
+                error.line, error.column
+            )
         }
-        ErrorTypes::InvalidComparison(type1, type2, line, column) => {
-            println!("Can't compare type {type1} with type {type2} [line {line}: column {column}]")
+        ErrorKind::InvalidExitCode => {
+            println!(
+                "Exit codes can only be ints [line {}: column {}]",
+                error.line, error.column
+            )
         }
-        ErrorTypes::InvalidExitCode(line, column) => {
-            println!("Exit codes can only be ints [line {line}: column {column}]")
+        ErrorKind::InvalidAssignment { type1, type2 } => {
+            println!(
+                "Can't assign variable with type {type1} to expression with type {type2} [line {}: column {}]",
+                error.line, error.column
+            )
         }
     }
 }
