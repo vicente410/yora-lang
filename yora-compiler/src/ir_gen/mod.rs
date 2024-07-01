dd016daf57a0c35d96df0f679e9d849159679be4
vicente410 <brunottvicente@gmail.com>
2024-07-01 17:36:05 +0100

    changed to struct pattern

--- yora-compiler/src/ir_gen/mod.rs
+++ yora-compiler/src/ir_gen/mod.rs
@@ -44,250 +44,270 @@ struct Nums {
     loops: u32,
 }
 
-pub fn generate_ir(
-    ast: Vec<Expression>,
-    type_table: &mut HashMap<String, String>,
-) -> (Vec<Ir>, &mut HashMap<String, String>) {
-    let mut inter_repr = Vec::new();
-    let mut tmp_vec = Vec::new();
-    let mut nums = Nums {
-        tmp: 0,
-        ifs: 0,
-        loops: 0,
-    };
-
-    for expr in ast {
-        get_value(&expr, &mut tmp_vec, &mut nums, type_table);
-        inter_repr.append(&mut tmp_vec);
-        tmp_vec.clear();
-    }
-
-    (inter_repr, type_table)
+pub struct IrGenerator {
+    type_table: HashMap<String, String>,
+    nums: Nums,
+    inter_repr: Vec<Ir>,
 }
 
-fn get_value(
-    expr: &Expression,
-    tmp_vec: &mut Vec<Ir>,
-    nums: &mut Nums,
-    type_table: &mut HashMap<String, String>,
-) -> String {
-    match &expr.kind {
-        ExpressionKind::Exit(val) => {
-            let arg = get_value(val, tmp_vec, nums, type_table);
-            tmp_vec.push(Ir::Exit { src: arg.clone() });
-            arg
+impl IrGenerator {
+    fn new(type_table: HashMap<String, String>) -> IrGenerator {
+        IrGenerator {
+            type_table,
+            nums: Nums {
+                tmp: 0,
+                ifs: 0,
+                loops: 0,
+            },
+            inter_repr: Vec::new(),
         }
-        ExpressionKind::Assign(ref dest, ref src) | ExpressionKind::Declare(ref dest, ref src) => {
-            let arg1 = get_value(dest, tmp_vec, nums, type_table);
-            let arg2 = get_value(src, tmp_vec, nums, type_table);
+    }
 
-            tmp_vec.push(Ir::Op {
-                dest: arg1.clone(),
-                src: arg2,
-                op: Op::Assign,
-            });
-
-            arg1
+    fn gen_ir(&mut self, ast: Vec<Expression>) {
+        for expr in ast {
+            self.get_value(&expr);
         }
-        ExpressionKind::Add(ref dest, ref src)
-        | ExpressionKind::Sub(ref dest, ref src)
-        | ExpressionKind::Mul(ref dest, ref src)
-        | ExpressionKind::Div(ref dest, ref src)
-        | ExpressionKind::Mod(ref dest, ref src)
-        | ExpressionKind::And(ref dest, ref src)
-        | ExpressionKind::Or(ref dest, ref src) => {
-            nums.tmp += 1;
-            let destination = format!("t{}", nums.tmp);
+    }
 
-            let arg1 = get_value(dest, tmp_vec, nums, type_table);
-            let arg2 = get_value(src, tmp_vec, nums, type_table);
-
-            tmp_vec.push(Ir::Op {
-                dest: destination.clone(),
-                src: arg2.clone(),
-                op: Op::Assign,
-            });
-
-            type_table.insert(destination.clone(), type_table[&arg2].clone());
-
-            tmp_vec.push(get_operation(expr, destination.clone(), arg1));
-            destination
-        }
-        ExpressionKind::Eq(ref dest, ref src)
-        | ExpressionKind::Neq(ref dest, ref src)
-        | ExpressionKind::Lt(ref dest, ref src)
-        | ExpressionKind::Leq(ref dest, ref src)
-        | ExpressionKind::Gt(ref dest, ref src)
-        | ExpressionKind::Geq(ref dest, ref src) => {
-            let arg1 = get_value(dest, tmp_vec, nums, type_table);
-            let arg2 = get_value(src, tmp_vec, nums, type_table);
-
-            tmp_vec.push(Ir::Op {
-                dest: arg1.clone(),
-                src: arg2,
-                op: Op::Cmp,
-            });
-
-            nums.tmp += 1;
-            let destination = format!("t{}", nums.tmp);
-
-            type_table.insert(destination.clone(), "bool".to_string());
-
-            tmp_vec.push(Ir::Set {
-                dest: destination,
-                cond: get_condition(expr),
-            });
-
-            format!("t{}", nums.tmp)
-        }
-        ExpressionKind::IntLit(int) => {
-            nums.tmp += 1;
-
-            let destination = format!("t{}", nums.tmp);
-
-            tmp_vec.push(Ir::Op {
-                dest: destination.clone(),
-                src: int.to_string(),
-                op: Op::Assign,
-            });
-
-            type_table.insert(destination.clone(), "int".to_string());
-
-            destination
-        }
-        ExpressionKind::BoolLit(bool) => {
-            nums.tmp += 1;
-            let destination = format!("t{}", nums.tmp);
-
-            tmp_vec.push(Ir::Op {
-                dest: destination.clone(),
-                src: if bool == "true" {
-                    "1".to_string()
-                } else {
-                    "0".to_string()
-                },
-                op: Op::Assign,
-            });
-
-            type_table.insert(destination.clone(), "bool".to_string());
-
-            destination
-        }
-        ExpressionKind::Identifier(id) => id.to_string(),
-        ExpressionKind::If(cond, seq) => {
-            // todo: remove current_ifs
-            nums.ifs += 1;
-            let current_ifs = nums.ifs;
-            let src = get_value(cond, tmp_vec, nums, type_table);
-
-            tmp_vec.push(Ir::JmpCond {
-                src,
-                label: format!("end_if_{}", current_ifs),
-            });
-
-            let seq_value = get_value(seq, tmp_vec, nums, type_table);
-
-            tmp_vec.push(Ir::Label(format!("end_if_{}", current_ifs)));
-
-            seq_value
-        }
-        ExpressionKind::IfElse(cond, if_seq, else_seq) => {
-            nums.ifs += 1;
-            let current_ifs = nums.ifs;
-
-            let src = get_value(cond, tmp_vec, nums, type_table);
-
-            tmp_vec.push(Ir::JmpCond {
-                src,
-                label: format!("else_{}", current_ifs),
-            });
-
-            let if_seq_value = get_value(if_seq, tmp_vec, nums, type_table);
-            tmp_vec.push(Ir::Jmp {
-                label: format!("end_if_{}", current_ifs),
-            });
-
-            tmp_vec.push(Ir::Label(format!("else_{}", current_ifs)));
-            get_value(else_seq, tmp_vec, nums, type_table);
-            tmp_vec.push(Ir::Label(format!("end_if_{}", current_ifs)));
-
-            if_seq_value
-        }
-        ExpressionKind::Loop(seq) => {
-            tmp_vec.push(Ir::Label(format!("loop_{}", nums.loops)));
-            let seq_value = get_value(seq, tmp_vec, nums, type_table);
-
-            tmp_vec.push(Ir::Jmp {
-                label: format!("loop_{}", nums.loops),
-            });
-
-            tmp_vec.push(Ir::Label(format!("loop_end_{}", nums.loops)));
-            nums.loops += 1;
-
-            seq_value
-        }
-        ExpressionKind::Break => {
-            tmp_vec.push(Ir::Jmp {
-                label: format!("loop_end_{}", nums.loops),
-            });
-            "".to_string()
-        }
-        ExpressionKind::Continue => {
-            tmp_vec.push(Ir::Jmp {
-                label: format!("loop_{}", nums.loops),
-            });
-            "".to_string()
-        }
-        ExpressionKind::Sequence(seq) => {
-            for expr in &seq[0..seq.len() - 1] {
-                get_value(expr, tmp_vec, nums, type_table);
+    fn get_value(&mut self, expr: &Expression) -> String {
+        match &expr.kind {
+            ExpressionKind::Exit(val) => {
+                let arg = self.get_value(val);
+                self.inter_repr.push(Ir::Exit { src: arg.clone() });
+                arg
             }
-            get_value(&seq[seq.len() - 1], tmp_vec, nums, type_table)
+            ExpressionKind::Assign(ref dest, ref src)
+            | ExpressionKind::Declare(ref dest, ref src) => {
+                let arg1 = self.get_value(dest);
+                let arg2 = self.get_value(src);
+
+                self.inter_repr.push(Ir::Op {
+                    dest: arg1.clone(),
+                    src: arg2,
+                    op: Op::Assign,
+                });
+
+                arg1
+            }
+            ExpressionKind::Add(ref dest, ref src)
+            | ExpressionKind::Sub(ref dest, ref src)
+            | ExpressionKind::Mul(ref dest, ref src)
+            | ExpressionKind::Div(ref dest, ref src)
+            | ExpressionKind::Mod(ref dest, ref src)
+            | ExpressionKind::And(ref dest, ref src)
+            | ExpressionKind::Or(ref dest, ref src) => {
+                self.nums.tmp += 1;
+                let destination = format!("t{}", self.nums.tmp);
+
+                let arg1 = self.get_value(dest);
+                let arg2 = self.get_value(src);
+
+                self.inter_repr.push(Ir::Op {
+                    dest: destination.clone(),
+                    src: arg2.clone(),
+                    op: Op::Assign,
+                });
+
+                self.type_table
+                    .insert(destination.clone(), self.type_table[&arg2].clone());
+
+                self.inter_repr
+                    .push(IrGenerator::get_operation(expr, destination.clone(), arg1));
+                destination
+            }
+            ExpressionKind::Eq(ref dest, ref src)
+            | ExpressionKind::Neq(ref dest, ref src)
+            | ExpressionKind::Lt(ref dest, ref src)
+            | ExpressionKind::Leq(ref dest, ref src)
+            | ExpressionKind::Gt(ref dest, ref src)
+            | ExpressionKind::Geq(ref dest, ref src) => {
+                let arg1 = self.get_value(dest);
+                let arg2 = self.get_value(src);
+
+                self.inter_repr.push(Ir::Op {
+                    dest: arg1.clone(),
+                    src: arg2,
+                    op: Op::Cmp,
+                });
+
+                self.nums.tmp += 1;
+                let destination = format!("t{}", self.nums.tmp);
+
+                self.type_table
+                    .insert(destination.clone(), "bool".to_string());
+
+                self.inter_repr.push(Ir::Set {
+                    dest: destination,
+                    cond: IrGenerator::get_condition(expr),
+                });
+
+                format!("t{}", self.nums.tmp)
+            }
+            ExpressionKind::IntLit(int) => {
+                self.nums.tmp += 1;
+
+                let destination = format!("t{}", self.nums.tmp);
+
+                self.inter_repr.push(Ir::Op {
+                    dest: destination.clone(),
+                    src: int.to_string(),
+                    op: Op::Assign,
+                });
+
+                self.type_table
+                    .insert(destination.clone(), "int".to_string());
+
+                destination
+            }
+            ExpressionKind::BoolLit(bool) => {
+                self.nums.tmp += 1;
+                let destination = format!("t{}", self.nums.tmp);
+
+                self.inter_repr.push(Ir::Op {
+                    dest: destination.clone(),
+                    src: if bool == "true" {
+                        "1".to_string()
+                    } else {
+                        "0".to_string()
+                    },
+                    op: Op::Assign,
+                });
+
+                self.type_table
+                    .insert(destination.clone(), "bool".to_string());
+
+                destination
+            }
+            ExpressionKind::Identifier(id) => id.to_string(),
+            ExpressionKind::If(cond, seq) => {
+                // todo: remove current_ifs
+                self.nums.ifs += 1;
+                let current_ifs = self.nums.ifs;
+                let src = self.get_value(cond);
+
+                self.inter_repr.push(Ir::JmpCond {
+                    src,
+                    label: format!("end_if_{}", current_ifs),
+                });
+
+                let seq_value = self.get_value(seq);
+
+                self.inter_repr
+                    .push(Ir::Label(format!("end_if_{}", current_ifs)));
+
+                seq_value
+            }
+            ExpressionKind::IfElse(cond, if_seq, else_seq) => {
+                self.nums.ifs += 1;
+                let current_ifs = self.nums.ifs;
+
+                let src = self.get_value(cond);
+
+                self.inter_repr.push(Ir::JmpCond {
+                    src,
+                    label: format!("else_{}", current_ifs),
+                });
+
+                let if_seq_value = self.get_value(if_seq);
+                self.inter_repr.push(Ir::Jmp {
+                    label: format!("end_if_{}", current_ifs),
+                });
+
+                self.inter_repr
+                    .push(Ir::Label(format!("else_{}", current_ifs)));
+                self.get_value(else_seq);
+                self.inter_repr
+                    .push(Ir::Label(format!("end_if_{}", current_ifs)));
+
+                if_seq_value
+            }
+            ExpressionKind::Loop(seq) => {
+                self.inter_repr
+                    .push(Ir::Label(format!("loop_{}", self.nums.loops)));
+                let seq_value = self.get_value(seq);
+
+                self.inter_repr.push(Ir::Jmp {
+                    label: format!("loop_{}", self.nums.loops),
+                });
+
+                self.inter_repr
+                    .push(Ir::Label(format!("loop_end_{}", self.nums.loops)));
+                self.nums.loops += 1;
+
+                seq_value
+            }
+            ExpressionKind::Break => {
+                self.inter_repr.push(Ir::Jmp {
+                    label: format!("loop_end_{}", self.nums.loops),
+                });
+                "".to_string()
+            }
+            ExpressionKind::Continue => {
+                self.inter_repr.push(Ir::Jmp {
+                    label: format!("loop_{}", self.nums.loops),
+                });
+                "".to_string()
+            }
+            ExpressionKind::Sequence(seq) => {
+                for expr in &seq[0..seq.len() - 1] {
+                    self.get_value(expr);
+                }
+                self.get_value(&seq[seq.len() - 1])
+            }
+            ExpressionKind::Not(arg) => {
+                self.nums.tmp += 1;
+                let destination = format!("t{}", self.nums.tmp);
+
+                let arg1 = self.get_value(arg);
+
+                self.inter_repr.push(Ir::Op {
+                    dest: destination.clone(),
+                    src: arg1.clone(),
+                    op: Op::Not,
+                });
+
+                self.type_table
+                    .insert(destination.clone(), "bool".to_string());
+
+                destination
+            }
         }
-        ExpressionKind::Not(arg) => {
-            nums.tmp += 1;
-            let destination = format!("t{}", nums.tmp);
+    }
 
-            let arg1 = get_value(arg, tmp_vec, nums, type_table);
+    fn get_operation(operation: &Expression, arg1: String, arg2: String) -> Ir {
+        Ir::Op {
+            dest: arg1,
+            src: arg2,
+            op: match operation.kind {
+                ExpressionKind::Add(..) => Op::Add,
+                ExpressionKind::Sub(..) => Op::Sub,
+                ExpressionKind::Mul(..) => Op::Mul,
+                ExpressionKind::Div(..) => Op::Div,
+                ExpressionKind::Mod(..) => Op::Mod,
+                ExpressionKind::And(..) => Op::And,
+                ExpressionKind::Or(..) => Op::Or,
+                _ => panic!("Unexpected operation"),
+            },
+        }
+    }
 
-            tmp_vec.push(Ir::Op {
-                dest: destination.clone(),
-                src: arg1.clone(),
-                op: Op::Not,
-            });
-
-            type_table.insert(destination.clone(), "bool".to_string());
-
-            destination
+    fn get_condition(expr: &Expression) -> Cond {
+        match &expr.kind {
+            ExpressionKind::Eq(..) => Cond::Eq,
+            ExpressionKind::Neq(..) => Cond::Neq,
+            ExpressionKind::Lt(..) => Cond::Lt,
+            ExpressionKind::Leq(..) => Cond::Leq,
+            ExpressionKind::Gt(..) => Cond::Gt,
+            ExpressionKind::Geq(..) => Cond::Geq,
+            _ => panic!("Invalid condition"),
         }
     }
 }
 
-fn get_operation(operation: &Expression, arg1: String, arg2: String) -> Ir {
-    Ir::Op {
-        dest: arg1,
-        src: arg2,
-        op: match operation.kind {
-            ExpressionKind::Add(..) => Op::Add,
-            ExpressionKind::Sub(..) => Op::Sub,
-            ExpressionKind::Mul(..) => Op::Mul,
-            ExpressionKind::Div(..) => Op::Div,
-            ExpressionKind::Mod(..) => Op::Mod,
-            ExpressionKind::And(..) => Op::And,
-            ExpressionKind::Or(..) => Op::Or,
-            _ => panic!("Unexpected operation"),
-        },
-    }
-}
+pub fn generate_ir(ast: Vec<Expression>, type_table: &mut HashMap<String, String>) -> Vec<Ir> {
+    let mut generator = IrGenerator::new(type_table.clone());
 
-fn get_condition(expr: &Expression) -> Cond {
-    match &expr.kind {
-        ExpressionKind::Eq(..) => Cond::Eq,
-        ExpressionKind::Neq(..) => Cond::Neq,
-        ExpressionKind::Lt(..) => Cond::Lt,
-        ExpressionKind::Leq(..) => Cond::Leq,
-        ExpressionKind::Gt(..) => Cond::Gt,
-        ExpressionKind::Geq(..) => Cond::Geq,
-        _ => panic!("Invalid condition"),
-    }
+    generator.gen_ir(ast);
+
+    generator.inter_repr
 }
